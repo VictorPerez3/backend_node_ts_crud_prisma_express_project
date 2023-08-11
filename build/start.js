a10c0f172971cd26e07d177eaebe91c641e4a961
victorperez3 <victorvagner@ucl.br>
2023-08-11 18:29:42 -0300

    doc/feat: dockercompose

---
+++ build/start.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const app_1 = require("./app");
+//Comando para inicialização do servidor pelo script "yarn start-dev)"
+(0, app_1.startServer)();
+//# sourceMappingURL=start.js.map
